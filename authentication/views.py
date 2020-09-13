f2eaa32fba286ff54726b11531de0dfd7330f9fc
Franklin okech <franklinokech@gmail.com>
2020-09-13 17:13:18 +0300

    finished ajax calls for registration page

--- authentication/views.py
+++ authentication/views.py
@@ -3,6 +3,7 @@ from django.views import View
 import json
 from django.http import JsonResponse
 from django.contrib.auth.models import User
+from validate_email import validate_email
 
 
 # define API endpoint to handle username validation
@@ -32,6 +33,38 @@ class UsernameValidationView(View):
             'username_valid': True,
         }
         return JsonResponse(context)
+
+
+# define API endpoint to validate email
+class EmailValidationView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+        email = data['email']
+
+        # Check if email is not valid
+        if not validate_email(email):
+            # Send back json with error
+            context = {
+                'email_error': 'Email address is invalid',
+            }
+            return JsonResponse(context, status=400)
+
+        # Check if email is already taken
+        if User.objects.filter(email=email).exists():
+            # Send back json with error
+            context = {
+                'email_error': 'Sorry,  the email address is taken, choose another one',
+            }
+            return JsonResponse(context, status=409)
+
+        # otherwise return true for valid email address
+        context = {
+            'email_valid': True,
+        }
+        return JsonResponse(context)
+
+
+
 
 
 # Define class based view for the register path
--- frankexpensetracker/static/js/register.js
+++ frankexpensetracker/static/js/register.js
@@ -2,21 +2,95 @@ console.log("register working correctly");
 
 // Get element usernameField by ID or classnames
 const usernameField = document.querySelector("#usernameField");
-const feedBackArea = document.querySelector(".invalid-feedback");
+const usernameFeedBackArea = document.querySelector(".usernameFeedbackArea");
+const emailField = document.querySelector("#emailField");
+const emailFeedBackArea = document.querySelector(".emailFeedbackArea");
+const usernameSuccesOutput = document.querySelector(".usernameSuccesOutput");
+const emailSuccesOutput = document.querySelector(".emailSuccesOutput");
+const passwordField = document.querySelector("#passwordField");
+const showPasswordToggle = document.querySelector(".showPasswordToggle");
 
-// Add Event listerner Key Up
-usernameField.addEventListener("keyup", (e) => {
+
+
+const handleToggleInput=(e)=>{
+    // change toggle to HIDE or Show depending on context
+    if(showPasswordToggle.textContent === "Show"){
+        showPasswordToggle.textContent = "Hide";
+
+        // update password field property to text
+        passwordField.setAttribute("type", "password");
+    }
+    else{
+        showPasswordToggle.textContent = "Show";
+
+        // update password field property to text
+        passwordField.setAttribute("type", "text");
+    }
+
+}
+
+showPasswordToggle.addEventListener("click", handleToggleInput);
+
+
+// Add Event listerner Key Up on emailField
+emailField.addEventListener("keyup", (e) => {
     console.log("typing", "typing...");
+    emailSuccesOutput.style.display = "block";
+
+    // capture user entered value
+    const emailVal = e.target.value;
+
+    emailSuccesOutput.textContent = `Checking...${emailVal}`;
+
+    console.log("emailVal", emailVal);
+
+
+// By defaul, the field are set to none
+     emailField.classList.remove("is-invalid");
+     emailFeedBackArea.style.display = "none";
+
+
+
+
+    // Check if email field has some value
+    if (emailVal.length > 0){
+        // set up a feth API to server
+        fetch("/authentication/validate-email", {
+            body: JSON.stringify({ email: emailVal }),
+            method: "POST",
+
+        }).then(res => res.json()).then(data => {
+            emailSuccesOutput.style.display = "none";
+            console.log("data", data);
+            
+            // if data has error, highlight the html field
+            if(data.email_error){
+                emailField.classList.add("is-invalid");
+                emailFeedBackArea.style.display = "block";
+                emailFeedBackArea.innerHTML = `<p>${data.email_error}</p>`
+            }
+        });        
+    }
+});
+
+
+// Add Event listerner Key Up on UsernameField
+usernameField.addEventListener("keyup", (e) => {
+    // console.log("typing", "typing...");
+
+    usernameSuccesOutput.style.display = "block";
+    
 
     // capture user entered value
     const usernameVal = e.target.value;
 
-    console.log("usernameVal", usernameVal);
+    // console.log("usernameVal", usernameVal);
+    usernameSuccesOutput.textContent = `Checking...${usernameVal}`;
 
 
 // By defaul, the field are set to none
      usernameField.classList.remove("is-invalid");
-     feedBackArea.style.display = "none";
+     usernameFeedBackArea.style.display = "none";
 
 
 
@@ -29,12 +103,13 @@ usernameField.addEventListener("keyup", (e) => {
             method: "POST",
 
         }).then(res => res.json()).then(data => {
+            usernameSuccesOutput.style.display = "none";
             console.log("data", data);
             // if data has error, highlight the html field
             if(data.username_error){
                 usernameField.classList.add("is-invalid");
-                feedBackArea.style.display = "block";
-                feedBackArea.innerHTML = `<p>${data.username_error}</p>`
+                usernameFeedBackArea.style.display = "block";
+                usernameFeedBackArea.innerHTML = `<p>${data.username_error}</p>`
             }
         });        
     }
